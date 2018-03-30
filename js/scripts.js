48bcd89016bd78f3e78c73ef63d59dedf09b03f9
Garnett Puett <gpuett@gmail.com>
2018-03-30 09:06:26 -0700

    Finish implementation of first spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
 // Business Logic
 var binary = ["0", "1"];
 function hal9000(number) {
-  var userArray = number.splt("");
-  var outputString0 = "<em>Beep!</em>";
+  var userArray = number.split("");
+  var outputString0 = "Beep!";
   var outputString1 = "<em>Boop!</em>";
   var outputString3 = "<em>I'm sorry, Dave. I'm afraid I can't do that.</em>";
-    for (var i = 0; i < userArray.length, i++) {
-
+  var robotOutput = "";
+    for (var i = 0; i < userArray.length; i++) {
+      if (userArray[i] === binary[0]) {
+        robotOutput = outputString0
+      }
     }
-  // return   
+  return robotOutput
 }
 
 
 
 // User-Interface Logic
 $(function() {
-  ("#robot").submit(function(event) {
+  $("#robot").submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($("#input").val());
+    var userInput = $("#input").val();
     var robotSpeech = hal9000(userInput);
-    $("#output").text(robotSpeech);
+    $("#output").append("<li>" + robotSpeech + "</li>");
   });
 });
