bcaf0850e8514eeb82644fac67fd9b019a944d14
Garnett Puett <gpuett@gmail.com>
2018-03-30 08:47:32 -0700

    start writing business logic function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // Business Logic
+var binary = ["0", "1"];
+function hal9000(number) {
+  var userArray = number.splt("");
+  var outputString0 = "<em>Beep!</em>";
+  var outputString1 = "<em>Boop!</em>";
+  var outputString3 = "<em>I'm sorry, Dave. I'm afraid I can't do that.</em>";
+    for (var i = 0; i < userArray.length, i++) {
 
-
-
+    }
+  // return   
+}
 
 
 
@@ -10,5 +18,7 @@ $(function() {
   ("#robot").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#input").val());
-  })
+    var robotSpeech = hal9000(userInput);
+    $("#output").text(robotSpeech);
+  });
 });
