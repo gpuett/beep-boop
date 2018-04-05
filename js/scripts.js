8b593f83d25f58e18145859292e1a5e83f1b50cb
Garnett Puett <gpuett@gmail.com>
2018-04-04 20:01:06 -0700

    fix styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,37 @@
-// Business Logic
-var binary = ["0", "1"];
+// business logic
 function hal9000(number) {
-  var userArray = number.split("");
   var beep = "Beep!";
   var boop = "Boop!";
-  var dave = "I'm sorry, ". I'm afraid I can't do that.""
-  var outputString3 =;
-  var name = $("#name").val();
-  var robotOutput = "";
-  var range = [];
-    for (var i = 0; i < userArray.length; i++) {
-      if (number % 3 === 0 && number > 0) {
-        robotOutput = outputString2 + name + outputString3
-        // $("body").css("background-color", "red");
-        // $(".jumbotron").css("background-image", "url(img/hal.jpg)");
-        // $("#twirl").show();
-        // $("#vortex").hide();
-        // $("#beep").hide();
-      } else if (userArray[i] === binary[1]) {
-        robotOutput = outputString1
-        // $("body").css("background-color", "black");
-        // $(".jumbotron").css("background-image", "url(img/hal1.jpg)");
-        // $("#beep").show();
-        // $("#vortex").hide();
-        // $("#twirl").hide();
-        break
-      } else if (userArray[i] === binary[0]) {
-        robotOutput = outputString0
-        // $("body").css("background-color", "black");
-        // $(".jumbotron").css("background-image", "url(img/hal1.jpg)");
-        // $("#beep").show();
-        // $("#vortex").hide();
-        // $("#twirl").hide();
+  var dave = "I'm sorry, Dave. I'm afraid I can't do that."
+  var userArray = [];
+    for (var i = 0; i <= number; i++) {
+      var userString = i.toString().split("");
+      if (i % 3 === 0 && i > 0) {
+        userArray.push(dave);
+      } else if (userString.includes("1")) {
+        userArray.push(boop);
+      } else if (userString.includes("0")) {
+        userArray.push(beep)
       } else {
-        for (var j = 0; j <= number; j++) {
-          range.push(j);
-          robotOutput = range
-          // $("body").css("background-color", "black");
-          // $(".jumbotron").css("background-image", "url(img/hal1.jpg)");
-          // $("#vortex").show();
-          // $("#twirl").hide();
-          // $("#beep").hide();
-        }
+        userArray.push(i)
       }
     }
-  return robotOutput
+  return userArray;
 }
 
-// User-Interface Logic
+// user interface logic
 $(function() {
   $("#robot").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#input").val();
+    var userInput = parseInt($("#input").val());
     var robotSpeech = hal9000(userInput);
-    $("#output").append("<li>" + robotSpeech + "</li>");
+    $("#output").append("<li>[" + robotSpeech + "]</li>");
   });
   $("#reverse").click(function(event) {
     event.preventDefault();
     var userInput = $("#input").val();
     var robotSpeech = hal9000(userInput);
     var reverseSpeech = robotSpeech.reverse();
-    $("#output").append("<li>" + reverseSpeech + "</li>");
-    $("body").css("background-color", "red");
+    $("#output").append("<li>[" + reverseSpeech + "]</li>");
   });
 });
