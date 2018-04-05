ac4a9d1d5c76236c9ff959b88a289dd64f4b61a3
Garnett Puett <gpuett@gmail.com>
2018-04-04 19:35:53 -0700

    refactor javascript backend code to split array and search for inclusive digits.

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,25 +1,23 @@
 // business logic
-var binary = ["0", "1"]
 function hal9000(number) {
   var beep = "Beep!";
   var boop = "Boop!";
   var dave = "I'm sorry, Dave. I'm afraid I can't do that."
-  var range = [];
-  var robotOutput = range;
+  var userArray = [];
     for (var i = 0; i <= number; i++) {
-      range.push(i);
-    } for (var j = 0; j < range.length; j++) {
-      if (range[j] === 0 || range[j].indexOf(0) !== -1 ) {
-        range[j] = beep;
-      } if (range[j] === 1) {
-        range[j] = boop;
-      } if (range[j] % 3 === 0) {
-        range[j] = dave;
+      var userString = i.toString().split("");
+      if (i % 3 === 0 && i > 0) {
+        userArray.push(dave);
+      } else if (userString.includes("1")) {
+        userArray.push(boop);
+      } else if (userString.includes("0")) {
+        userArray.push(beep)
+      } else {
+        userArray.push(i)
       }
     }
-
-console.log(range);
-  return robotOutput;
+console.log(userArray);
+  return userArray;
 }
 
 
